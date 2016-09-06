236269e09b2738ff53cd018effadfbf392e7aa1f
Chris Wilson <wilsonc1@live.co.uk>
2016-09-06 14:18:32 +0100

    Initial commit of CartThrob

---
+++ README.md
@@ -0,0 +1,32 @@
+Compatibility
+=========================
+
+Compatible with CartThrob version 3 and Expression Engine version 3
+
+Installation Instructions
+=========================
+
+**Step 1:**
+
+Copy the contents of the httpdocs folder to your root Expression Engine directory, clicking "Yes" merge folders
+
+**Step 2:**
+
+Log in to the admin area of Expression Engine, then navigate to the developer tools (Wrench icon) in the top right.
+Then click "Add-on Manager".
+Locate your 'CartThrob Add-on' in the "Thrid Party Add-Ons" section and click manage.
+Navigate to and click the "PAYMENTS" tab.
+
+**Step 3:**
+
+In the "Select a gateway to edit its settings" dropdown choose "Cardstream (Hosted)"
+
+**Step 4:**
+
+Configure the "Cardstream (Hosted)" payment gateway settings. Once configured, scroll down and click submit. The module has now been added to the checkout page.
+
+**Module Configuration:**
+
+By default we have set the module to the use the test details. To use your live details simply replace the test details in the text boxes with the ones supplied from your Cardstream live letter.
+
+![Cardstream Config settings](/images/cardstream-cartthrob-configuration.png)
---
+++ httpdocs/system/user/addons/cartthrob/third_party/language/english/cartthrob_cardstream_hosted_lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang = array(
+  'cardstream_title' => 'Cardstream (Hosted)',
+  'cardstream_description' => '<a href="https://www.cardstream.com"><img src="system/user/addons/cartthrob/third_party/payment_gateways/cs-logo.png" border="0" alt="Cardstream" /></a>
+  <p> At Cardstream, we have over 15 years experience of working with merchants sharing the same vision to grow their business online. As a trusted Online Payment Gateway partner to thousands of merchants, you as the merchant can have peace of mind in knowing our highest level of security delivers each and every transaction to your bank account safely.</p>
+  <p>Cardstream offers the most advanced and secure Level 1 PCI:DSS compliant online payment processing to thousands of merchants and in turn millions of cardholders. Our Online Payment Gateway is highly flexible and can be integrated easily into numerous shopping cart systems. From startups to global corporations and charities, we have the online payment solution for your needs. Start accepting debit and credit card payments online and over the phone with our guaranteed lowest rates here.</p>
+  <p><a href="https://www.cardstream.com/signup">Signup for an account</a></p>
+  <p><a href="https://mms.cardstream.com/">Login to the Cardstream merchant area</a></p>
+  <p>Accept credit cards through the Cardstream payment gateway.</p>
+  <p>This gateway makes use of a file called extload.php which is contained in the CartThrob themes folder.</p>',
+  'cardstream_merchant_id' => 'Merchant ID',
+  'cardstream_signature_key' => 'Signature Key',
+  'cardstream_form_responsive' => 'Responsive Form',
+  'cardstream_custom_url' => 'Custom Gateway URL',
+  'cardstream_currency_code' => 'Currency Code',
+  'cardstream_country_code' => 'Country Code'
+);
+?>
